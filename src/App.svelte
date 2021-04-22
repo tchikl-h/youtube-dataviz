15f1a645dd9e4a3ec55b379fe5a6c937645ca23a
Hervé Tchikladzé <htchikladze@ippon.fr>
2021-04-22 16:50:16 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<!-- App.svelte -->
+<script>
+	import { Router, Route } from "svelte-routing";
+  
+	// Admin Layout
+	import Admin from "./layouts/Admin.svelte";
+	// Auth Layout
+	import Auth from "./layouts/Auth.svelte";
+  
+	export let url = "";
+  </script>
+  
+  <Router url="{url}">
+	<!-- admin layout -->
+	<Route path="/" component="{Admin}" />
+	<!-- auth layout -->
+	<Route path="auth/*auth" component="{Auth}" />
+  </Router>
